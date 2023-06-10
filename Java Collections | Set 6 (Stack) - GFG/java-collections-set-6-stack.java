9d64826f13e6d8c00fb078451d4fb421e1b50755
UTKARSH KAUSHIK <89575063+UTKARSH907@users.noreply.github.com>
2023-06-10 14:34:01 +0530

    Added solution - LeetHub

---
+++ Java Collections | Set 6 (Stack) - GFG/java-collections-set-6-stack.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException{
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int n = Integer.parseInt(in.readLine());
+            String a[] = in.readLine().trim().split("\\s+");
+            int arr[] = new int[n];
+            for(int i = 0;i < n;i++)
+                arr[i] = Integer.parseInt(a[i]);
+            Stack<Integer> st = new Stack<>();
+            for(int i = 0;i < n;i++)
+                st.push(arr[i]);
+            
+            Solution ob = new Solution();
+            Stack<Integer> ans = new Stack<>();
+            ans = ob.deleteMid(n, st);
+            while(!ans.empty()) {
+                int x = ans.pop();
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution{
+    static Stack<Integer> deleteMid(int n, Stack<Integer> st)
+    {
+        // code here
+        Stack<Integer> s=new Stack<>();
+        // System.out.println(n/2);
+        for(int i=0;i<n;i++){
+            if(i==n/2){
+                st.pop();
+                continue;
+            }
+            s.push(st.pop());
+        }
+        Stack<Integer> res=new Stack<>();
+        while(s.size()>0){
+            res.push(s.pop());
+        }
+        return res;
+    }
+}
